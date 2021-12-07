f76f08ef34e6fd93697e7ab8d54728fab4528668
jackarms <90922918+jackarms@users.noreply.github.com>
2021-12-06 18:56:14 -0600

    more work

--- assets/script.js
+++ assets/script.js
@@ -2,129 +2,80 @@ var theQuestions = [
     {
         id: 1,
         question: "Which of the following is NOT an HTML tag",
-        answers: {
-            a: "<p>",
-            b: "<h1>",
-            c: "<body>",
-            d: "<c1>",
-        },
-        correctAnswer: "d"
+        answers: ["<p>", "<h1>", "<body>", "<c1>"],
+        correctAnswer: "<c1>"
     },
     {
         id: 2,
         question: "HTML was originally designed by who?",
-        answers: {
-            a: "Tim Berners-Lee",
-            b: "Bill Gates",
-            c: "Steve Jobs",
-            d: "That one guy from facebook",
-        },
-        correctAnswer: "a"
+        answers: ["Tim Berners-Lee", "Bill Gates", "Steve Jobs", "That one guy from facebook"],
+        correctAnswer: "Tim Berners-Lee"
     },
     {
         id: 3,
         question: "Why is the <alt> attribute so important to the <img> tag?",
-        answers: {
-            a: "In case the image path is broken",
-            b: "Slow network prevents image from loading",
-            c: "Users with visual impairments can still enjoy the webpage",
-            d: "All the above",
-        },
-        correctAnswer: "d"
+        answers: ["In case the image path is broken", "Slow network prevents image from loading", "Users with visual impairments can still enjoy the webpage", "All the above"],
+        correctAnswer: "All the above"
     },
     {
         id: 4,
         question: "Currently, which version of HTML do we use?",
-        answers: {
-            a: "HTML6",
-            b: "HTML5",
-            c: "HTML99.99",
-            d: "HTML68",
-        },
-        correctAnswer: "b"
+        answers: ["HTML6", "HTML5", "HTML99.99", "HTML68"],
+        correctAnswer: "HTML5"
     },
     {
         id: 5,
         question: "Meta information could be described as?",
-        answers: {
-            a: "Facebook's ability to see what you are thinking",
-            b: "Information shown by the web browser",
-            c: "Information about the web page",
-            d: "A useful component of JavaScript",
-        },
-        correctAnswer: "c"
+        answers: ["Facebook's ability to see what you are thinking", "Information shown by the web browser", "Information about the web page", "A useful component of JavaScrip"],
+        correctAnswer: "Information about the webpage"
     },
     {
         id: 6,
         question: "CSS is used to do what?",
-        answers: {
-            a: "Provide the structure of a webpage",
-            b: "Make a webpage interactive",
-            c: "Change how a webpage looks",
-            d: "Almost nothing, it is practically useless"
-        },
-        correctAnswer: "c"
+        answers: ["Provide the structure of a webpage", "Make a webpage interactive", "Change how a webpage looks", "Almost nothing, it is practically useless"],
+        correctAnswer: "Change how a webpage looks"
     },
     {
         id: 7,
         question: "Is JavaScript the same thing as Java",
-        answers: {
-            a: "No",
-            b: "Related, but not the same",
-            c: "Yes, Java is simply shorthand for JavaScript",
-            d: "Different versions of the same thing, like coke and pepsi",
-        },
-        correctAnswer: "a"
+        answers: ["No", "Related, but not the same", "Yes, Jave is simply shorthand for JavaScript", "Different versions of the same thing, like coke and pepsi"],
+        correctAnswer: "No"
     },
     {
         id: 8,
         question: "What is JavaScript used for?",
-        answers: {
-            a: "Script for brewing coffee",
-            b: "Making websites interactive",
-            c: "Adding style to webpages",
-            d: "Providing the structure for webpages",
-        },
-        correctAnswer: "b"
+        answers: ["Script for brewing coffee", "Making websites interactive", "Adding style to webpages", "Providing the structure for webpages"],
+        correctAnswer: "Making websites interactive"
     },
     {
         id: 9,
         question: "Which of the following is not considered one the three core technologies of the world wide web?",
-        answers: {
-            a: "JavaScript",
-            b: "CSS",
-            c: "HTML",
-            d: "Twitter",
-        },
-        correctAnswer: "d"
+        answers: ["JavaScript", "CSS", "HTML", "Twitter"],
+        correctAnswer: "Twitter"
     },
     {
         id: 10,
         question: "Bootstrap was designed by who?",
-        answers: {
-            a: "Facebook",
-            b: "Twitter",
-            c: "Instagram",
-            d: "None of the above",
-        },
-        correctAnswer: "b"
+        answers: ["Facebook", "Twitter", "Instagram", "None of the above"],
+        correctAnswer: "Twitter"
     },
 ]
+let usedQuestions = theQuestions.question;
 var startingPoints = 0
-
+var questions;
+var questionIndex = 0
 var buttonEl = document.querySelector('#begin-quiz');
-var questionEl = document.getElementById('questions');
 var answersEl = document.getElementById('answers');
 var firstPageEl = document.getElementById('opening')
 
 var showQuestions = function(){
     firstPageEl.remove();
-    for(var i=0; i < theQuestions.length; i++){
-        theQuestions[i].question = questionsEl;
-        theQuestions[i].answers = answerEl;
-        questionEl.appendChild(questionsEl)
-        }
-    }
+    //var questionEl= theQuestions[question];
+    //question = JSON.stringify(question)
+    var divQuestion = document.getElementById("questions");
+    divQuestion.innerHTML = usedQuestions
+    for(var i=0; i < theQuestions.length; i++){}
+}
 var theResults = function(){
 
 }
