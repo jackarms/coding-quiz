2c13749f3089343aba81d8971b85d2bf51f24960
jackarms <90922918+jackarms@users.noreply.github.com>
2021-12-10 17:00:47 -0600

    addition of radio buttons

--- assets/script.js
+++ assets/script.js
@@ -51,10 +51,16 @@ var questionsAndAnswers = [
     }
 ]
 
-var questionIndex = 0
-var buttonEl = document.querySelector('#begin-quiz');
+var theCorrectAnswer = questionsAndAnswers.map(x => x.correctAnswer)
+console.log(theCorrectAnswer)
+var theCorrectAnswerString = theCorrectAnswer.toString();
+console.log(theCorrectAnswerString)
+let questionIndex = 0
+let answerIndex = 0
+let theAnswers;
+var buttonBegin = document.querySelector('#begin-quiz');
 var questionsEl = document.getElementById('questions')
-var answersEl = document.getElementById('answers');
+var answersDiv = document.getElementById('answers');
 var answerOl = document.getElementById('answer-list');
 var firstPageEl = document.getElementById('opening');
 
@@ -63,21 +69,47 @@ var showQuestions = function() {
 
     questionsAndAnswers.forEach((q, index, arr) => {
         questionsEl.innerText = arr[questionIndex].question;
+        if(radioEl.onclick()) {
+            questionIndex++
+        }
         });
-    // we need to empty what was there before
-
-    // we need to display questions DONE
-
-    // we need to create h1 element then append it to document
 }
 
 var showAnswers = function() {
-
+    questionsAndAnswers[answerIndex].answers.forEach(answer => {
+         
+       let theAnswers = answer;
+       console.log(theAnswers)
+       let ulEl = document.createElement('ul');
+       let liEl = document.createElement('li');
+       let radioEl = document.createElement('INPUT');
+       radioEl.setAttribute('type', 'radio')
+       radioEl.setAttribute('id', 'radio');
+       radioEl.setAttribute('value', 'name');
+       radioEl.setAttribute('name', 'radioName');
+       radioEl.setAttribute('onclick', 'theClick()');
+       let lblEl = document.createElement('label')
+       lblEl.appendChild(document.createTextNode(theAnswers));
+       liEl.appendChild(lblEl);
+       liEl.appendChild(radioEl);
+       ulEl.appendChild(liEl);
+       answersDiv.appendChild(ulEl);
+       
+    radioEl.onclick = function () {
+        
+        if (document.getElementById('radio').checked ==true){
+            console.log("true");
+            questionIndex++
+            answerIndex++
+        } else {
+            console.log("false");
+        }
+    }
+    })
 }
 
 
-var rightOrWrong = function() {
 
-}
 
-buttonEl.addEventListener("click", showQuestions);
+buttonBegin.addEventListener("click", showQuestions);
+buttonBegin.addEventListener("click", showAnswers);
